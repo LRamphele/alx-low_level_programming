a65643e3d412bdaceee0c25d9f094209ea89561a
Lebogang <ramphele.lebogang@gmail.com>
2022-06-02 09:27:29 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Prints quote
+  * main - Prints quote
  *
  * Return: zero on success
  *
  */
-int main(voids)
+int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
     return (0);
