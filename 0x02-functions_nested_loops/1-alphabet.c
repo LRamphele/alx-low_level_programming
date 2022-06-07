e56a368ca688a99d90ce1316a2e39f7e581755c9
Lebogang <ramphele.lebogang@gmail.com>
2022-06-07 11:45:37 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet, in lowercase
+ * followed by a new line
+ */
+void print_alphabet(void)
+{
+char ch;
+ch = 'a';
+while (ch <= 'z')
+{
+_putchar(ch);
+ch++;
+}
+_putchar('\n');
+}
