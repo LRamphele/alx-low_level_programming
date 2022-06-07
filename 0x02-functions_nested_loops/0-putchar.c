ebde6061f33ce318156fc04b86d78a1ece7df7e8
Lebogang <ramphele.lebogang@gmail.com>
2022-06-07 05:54:56 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point 
+ * Return: 0
+ */
+int main(void)
+{
+char c[0] = "_putchar";
+int i = 0;
+while (i < 9)
+{
+_putchar(c[i]);
+i++;
+}
+_putchar('\n');
+return (0);
+}
