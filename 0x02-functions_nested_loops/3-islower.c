3db88f824d491850581db5a1ba3a3b0cf65d4121
Lebogang <ramphele.lebogang@gmail.com>
2022-06-07 13:23:42 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include <main.h>
+
+/**
+ * _islower - returns 1 if char is lowercase
+ * @c: character to be checked if lower case, type int
+ * return: 1 if lower case, 0 otherwise
+**/
+int _islower(int c)
+{
+if (c >=97 && c <= 122)
+return (1);
+else
+return (0);
+}
