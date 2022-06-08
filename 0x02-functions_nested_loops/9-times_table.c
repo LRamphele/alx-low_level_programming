6ff362cd23c47c140e146a0bed8368f835c1f70e
LRamphele <105162025+LRamphele@users.noreply.github.com>
2022-06-08 18:19:33 +0200

    Create 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include "stdio.h"
+
+/**
+ * times_table - prints times table
+ * Return:void
+ */
+void times_table(void)
+{
+int a = 0;
+int b;
+int rep;
+while (a <= 9)
+{
+	b = 0;
+	while (b <= 9)
+	{
+		rep = a * b;
+		if (b == 0)
+		{
+			putchar('0' + rep);
+		}
+		else if (rep < 10)
+		{
+			putchar(' ');
+			putchar('0' + rep);
+		}
+		else
+		{
+			putchar('0' + rep / 10);
+		  putchar('0' + rep % 10);
+		}
+
+		if (b < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		b++;
+	}
+	putchar('\n');
+	a++;
+}
+
+}
