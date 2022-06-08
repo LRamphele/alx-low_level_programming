904ad6e9ffd69fcd1c6d02cb681840c0d50b3c95
LRamphele <105162025+LRamphele@users.noreply.github.com>
2022-06-08 17:56:33 +0200

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -15,3 +15,15 @@ void print_alphabet(void)
   }
   putchar('\n');
 }
+
+#include "main.h"
+
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
