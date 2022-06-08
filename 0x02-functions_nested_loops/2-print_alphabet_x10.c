077378cabe6f25946b06716555a6435f64e2a987
LRamphele <105162025+LRamphele@users.noreply.github.com>
2022-06-08 17:59:42 +0200

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,21 +6,18 @@
  **/
 void print_alphabet(void)
 {
-char i;
-for (i = 'a'; i <= 'z'; i++)
-{
-putchar(i);
-}
-putchar('\n');
-}
-/**
- * print_alphabet_x10 - prints alphabet 10 times
- **/
-void print_alphabet_x10(void)
-{
-int i;
-for (i = '0'; i <= '9'; i++)
-{
-print_alphabet();
-}
+	char c;
+	int i = 0;
+
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
 }
