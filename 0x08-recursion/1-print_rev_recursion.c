a64851d975539a5b24b9a78fb16369afab5c2fda
LRamphele <105162025+LRamphele@users.noreply.github.com>
2022-06-21 18:20:52 +0200

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * _print_rev_recursion - prints a string in reverse
+ * print_rev_recursion - prints a string in reverse
  * @s: string to print
  */
 void _print_rev_recursion(char *s)
