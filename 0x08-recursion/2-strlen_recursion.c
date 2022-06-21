530532066478b15981d2cc1db65aabc9cd471335
LRamphele <105162025+LRamphele@users.noreply.github.com>
2022-06-21 18:34:19 +0200

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,11 +3,10 @@
 /**
  * _strlen_recursion - returns the length of a string
  * @s: string to calculate the length of
- * Return: length of the string
+ * Return: int
  */
 int _strlen_recursion(char *s)
 {
-int n = 0;
 if (*s == '\0')
 return (0);
 return (1 + _strlen_recursion(s + 1));
