daf7d3f44ad6527fd354ca5fed07f137d8087cb7
LRamphele <105162025+LRamphele@users.noreply.github.com>
2022-06-21 19:35:09 +0200

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,4 +1,4 @@
-#include "stdio.h>
+#include <stdio.h>
 
 int bandersnatch(char *s1, char *s2);
 char *move(char *s2);
@@ -8,7 +8,6 @@ char *move(char *s2);
  * checking for wildcards expansion
  * @s1: first string to compare
  * @s2: second string to compare
- *
  * Return: 1 if the strings can be considered identical
  * otherwise 0
  */
