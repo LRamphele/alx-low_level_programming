f12b19fc00ffb02f5e851a47904e9e0d694f9aeb
LRamphele <105162025+LRamphele@users.noreply.github.com>
2022-06-21 19:03:38 +0200

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,15 +5,16 @@
  * @s: string to print
  * Return: void
  */
-void_puts_recursion(char *s)
+void _puts_recursion(char *s)
 {
 if (*s == '\0')
 {
-_putchar('\n');
+putchar('\n');
 }
 else
 {
-_putchar(*s);
-_puts_recursion(s + 1);
+putchar(*s);
+puts_recursion(s + 1);
+}
 }
 }
